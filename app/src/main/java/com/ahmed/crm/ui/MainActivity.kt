921e53ae338ae6ca59a5006821f06ebb582ca0cd
Ahmed Zaki <a.zaki@silkysys.com>
2022-11-29 14:27:31 +0200

    Initial commit

---
+++ app/src/main/java/com/ahmed/crm/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ahmed.crm.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ahmed.crm.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
